fdab76d00db5f18c490e43d61ea96f84ccb4dc39
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-27 18:34:10 +0100

    feat: App > header

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,6 +2,9 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../components/App";
 
+const title = "Rick and Morty";
+const tagline = `"I'm a scientist because I invent, transform, create, and destroy for a living, and when I don't like something about the world, I change it."`;
+
 const renderApp = () => render(<App />);
 
 describe(`App`, () => {
@@ -11,4 +14,15 @@ describe(`App`, () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
+  it(`displays a heading with the text ${title}`, () => {
+    renderApp();
+
+    expect(screen.getByText(title)).toBeInTheDocument();
+  });
+
+  it(`displays a tagline with the text: ${tagline}`, () => {
+    renderApp();
+
+    expect(screen.getByText(tagline)).toBeInTheDocument();
+  });
 });
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,15 @@ import styles from "../styles/app.module.css";
 const App = () => {
   return (
     <div className={styles.app}>
+      <header className={styles.app__header}>
+        <h1 className={styles.app__heading}>Rick and Morty</h1>
+        <p className={styles.app__tagline}>
+          {`"I'm a scientist because I invent, transform, create, and destroy for
+          a living, and when I don't like something about the world, I change
+          it."`}
+        </p>
+      </header>
+      <main className={styles.app__main}>Test</main>
     </div>
   );
 };
