2ecd58b49b6223ecfc3815026887d74366a943bd
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-28 15:42:17 +0100

    feat: CharacterCard
    Renders 'alive' as green color and 'dead' as red color

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 import styles from "../styles/CharacterCard.module.css";
 
 const CharacterCard = ({ name, species, status, image }) => {
+  let statusColor = "currentColor";
+  if (status.toLowerCase() === "dead") statusColor = "#f00";
+  if (status.toLowerCase() === "alive") statusColor = "#0f0";
+
   return (
     <div className={styles["character-card"]}>
       <img className={styles["character-card__image"]} src={image} alt={name} />
@@ -17,11 +21,7 @@ const CharacterCard = ({ name, species, status, image }) => {
         </p>
         <p className={styles["character-card__stat"]}>
           <span className={styles["character-card__label"]}>Status: </span>
-          <span
-            style={{ color: `${status.toLowerCase() === "dead" ? "red" : ""}` }}
-          >
-            {status}
-          </span>
+          <span style={{ color: statusColor }}>{status}</span>
         </p>
       </div>
     </div>
